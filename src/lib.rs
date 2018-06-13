7b83afd21e53ea814d0ada1edfb1276b080046f8
Árpád Goretity <h2co3@h2co3.org>
2018-06-13 19:50:17 +0200

    Skeleton of event iterator

--- src/lib.rs
+++ src/lib.rs
@@ -25,3 +25,56 @@
                  print_stdout, mem_forget, maybe_infinite_iter))]
 
 extern crate pulldown_cmark;
+
+use std::fmt;
+use std::io;
+use std::borrow::Cow;
+use pulldown_cmark::{ Parser, Options, Event };
+
+/// Iterator adapter converting a stream of Markdown events to a stream of LaTeX.
+#[derive(Debug, Clone, Copy)]
+pub struct LaTeXIter<'a, T: Iterator<Item=Event<'a>>> {
+    /// The iterator being wrapped.
+    events: T,
+}
+
+impl<'a, T: Iterator<Item=Event<'a>>> LaTeXIter<'a, T> {
+    /// Wrap a Markdown `Event` iterator, turning it into a LaTeX stream.
+    pub fn new(events: T) -> Self {
+        LaTeXIter { events }
+    }
+
+    /// Convenience method for writing all output to an `fmt::Write`.
+    pub fn write_to_fmt<W: fmt::Write>(&mut self, dest: &mut W) -> fmt::Result {
+        for string in self {
+            dest.write_str(&string)?
+        }
+
+        Ok(())
+    }
+
+    /// Convenience method for writing all output to an `io::Write`.
+    pub fn write_to_io<W: io::Write>(&mut self, dest: &mut W) -> io::Result<()> {
+        for string in self {
+            dest.write_all(string.as_bytes())?
+        }
+
+        Ok(())
+    }
+}
+
+impl<'a> LaTeXIter<'a, Parser<'a>> {
+    /// Create a LaTeX event iterator from a Markdown string,
+    /// with all `Parser` options enabled.
+    pub fn with_str(string: &'a str) -> Self {
+        Self::new(Parser::new_ext(string, Options::all()))
+    }
+}
+
+impl<'a, T: Iterator<Item=Event<'a>>> Iterator for LaTeXIter<'a, T> {
+    type Item = Cow<'a, str>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        None
+    }
+}
