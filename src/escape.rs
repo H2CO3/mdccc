3bd661d5dcd6ce61098c140e51ca86d9f1403124
Árpád Goretity <h2co3@h2co3.org>
2018-07-14 23:27:02 +0200

    Add _some_ escaping

--- src/escape.rs
+++ src/escape.rs
@@ -2,7 +2,16 @@
 
 use std::borrow::Cow;
 
-/// LaTeX-escapes arbitrary text. TODO(H2CO3): implement me
+/// LaTeX-escapes arbitrary text. TODO(H2CO3): are there more
+/// characters we need to care about?
 pub fn latex(text: Cow<str>) -> Cow<str> {
-    text
+    if text.contains(['#', '%', '_'].as_ref()) {
+        text
+            .replace('#', r"\#")
+            .replace('%', r"\%")
+            .replace('_', r"\_")
+            .into()
+    } else {
+        text
+    }
 }
